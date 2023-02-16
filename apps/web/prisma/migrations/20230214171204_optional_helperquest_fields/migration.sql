d5e1f4e5b7514f998c6f6ad9bf0c84693449e8b8
Hugues Maignol <hugues@kime.tech>
2023-02-16 19:50:09 +0100

    feat: initial migration

--- apps/web/prisma/migrations/20230214171204_optional_helperquest_fields/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "HelpRequest" ALTER COLUMN "financialSupport" DROP NOT NULL,
-ALTER COLUMN "externalStructure" DROP NOT NULL;
--- apps/web/prisma/migrations/20230215151758_retirement/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `pensionStructure` column on the `Beneficiary` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Beneficiary" DROP COLUMN "pensionStructure",
-ADD COLUMN     "pensionStructure" TEXT[];
