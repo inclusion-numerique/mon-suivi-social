7f944640787a9047bc757c1541211cb585fc47d3
Hugues Maignol <hugues@kime.tech>
2023-02-16 11:14:49 +0100

    feat: scroll to item on history tab

---
+++ apps/web/src/app/(private)/beneficiaires/[fileNumber]/ScrollToSupportItem.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useEffect } from 'react'
+import { isBrowser } from '@mss/web/utils/isBrowser'
+import smoothScrollIntoView from 'smooth-scroll-into-view-if-needed'
+
+// Insert this component to trigger a smooth scroll to a support item card (followup or help request)
+export const ScrollToSupportItem = ({ item }: { item?: string }) => {
+  useEffect(() => {
+    if (!item || !isBrowser) {
+      return
+    }
+    const element = document.getElementById(item)
+
+    if (!element) {
+      return
+    }
+
+    smoothScrollIntoView(element, {
+      scrollMode: 'always',
+      behavior: 'smooth',
+      block: 'center',
+      inline: 'center',
+    })
+  }, [item])
+
+  return null
+}
