1b4ce4de5d0dd8f6ac33d6e1ba50e5c331b0cd1d
Hugues Maignol <hugues@kime.tech>
2023-02-22 15:22:16 +0100

    ci: add buffer for failing domain validations on scaleway

--- packages/cdk/src/utils.ts
+++ packages/cdk/src/utils.ts
@@ -30,8 +30,13 @@ export const createPreviewSubdomain = (
   previewDomain: string,
 ) => {
   // DNS record has to be 63 chars or shorter
-  // We will add a ".", so 62
-  const maxNamespaceLength = 62 - previewDomain.length
+  const maxRecordLength = 63
+  // But validation seems to fail for shorter records so we add a buffer
+  const validationFailureBuffer = 4
+  // We will add a ".", also remove 1
+  const maxNamespaceLength =
+    maxRecordLength - validationFailureBuffer - 1 - previewDomain.length
+
   if (namespace.length > maxNamespaceLength) {
     return `${namespace.slice(0, maxNamespaceLength)}.${previewDomain}`
   }
