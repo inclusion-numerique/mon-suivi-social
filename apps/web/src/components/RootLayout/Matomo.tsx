d410c77781dba2c53b42893de04c60119ec2998f
Mlbiche <tmlb@pm.me>
2023-03-07 10:30:08 +0100

    fix: remove matomo inline script

--- apps/web/src/components/RootLayout/Matomo.tsx
+++ apps/web/src/components/RootLayout/Matomo.tsx
@@ -4,17 +4,8 @@ import Script from 'next/script'
 
 export function Matomo() {
   return process.env.NODE_ENV === 'production' ? (
-    <Script id="matomo">
-      {`var _paq = window._paq = window._paq || [];
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-    var u="//matomo.incubateur.anct.gouv.fr/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '5']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();`}
-    </Script>
+    // FIXME: Is id useful ?
+    // FIXME: We could also leave inline script and add a nonce
+    <Script id="matomo" src="/matomo/mtomo.js" strategy="lazyOnload" />
   ) : null
 }
