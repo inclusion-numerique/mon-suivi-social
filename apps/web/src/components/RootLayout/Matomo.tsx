d0a27a7b31a5bb93bf43df9b0bd4353e0f58e8ee
Mlbiche <tmlb@pm.me>
2023-03-07 10:34:51 +0100

    fix: matomo script path

--- apps/web/src/components/RootLayout/Matomo.tsx
+++ apps/web/src/components/RootLayout/Matomo.tsx
@@ -6,6 +6,6 @@ export function Matomo() {
   return process.env.NODE_ENV === 'production' ? (
     // FIXME: Is id useful ?
     // FIXME: We could also leave inline script and add a nonce
-    <Script id="matomo" src="/matomo/mtomo.js" strategy="lazyOnload" />
+    <Script id="matomo" src="/matomo/matomo.js" strategy="lazyOnload" />
   ) : null
 }
