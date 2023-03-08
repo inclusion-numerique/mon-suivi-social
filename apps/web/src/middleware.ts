9baefe3259c436806681278b08e95a2d21f963cd
Hugues Maignol <hugues@kime.tech>
2023-03-08 10:16:58 +0100

    chore: CSP Report only while fixing remaining bugs in worker and webpack scripts

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -95,7 +95,7 @@ const middleware = (request: NextRequest) => {
   response.headers.delete('X-Powered-By')
   response.headers.append('Strict-Transport-Security', 'max-age=63072000')
 
-  response.headers.append('Content-Security-Policy', securityPolicy)
+  response.headers.append('Content-Security-Policy-Report-Only', securityPolicy)
 
   return response
 }
