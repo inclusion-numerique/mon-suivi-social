85a72988b7a64201df671c8e261a4b14a7756421
Hugues Maignol <hugues@kime.tech>
2023-03-09 08:52:02 +0100

    chore: allow module mocking compatibility with @swc/jest and update middleware test

--- apps/web/src/middleware.spec.ts
+++ apps/web/src/middleware.spec.ts
@@ -1,26 +1,44 @@
 import { NextRequest } from 'next/server'
 import middleware from './middleware'
+import * as generateContentSecurityPolicyScriptNonceModule from '@mss/web/utils/generateContentSecurityPolicyScriptNonce'
 
 describe('middleware', () => {
   const originalEnv = process.env
+  const nonceGeneratorSpy = jest.spyOn(
+    generateContentSecurityPolicyScriptNonceModule,
+    'generateContentSecurityPolicyScriptNonce',
+  )
 
   afterEach(() => {
     process.env = originalEnv
+    nonceGeneratorSpy.mockReset()
   })
 
   it('Adds CSP headers in development environment', () => {
+    nonceGeneratorSpy.mockReturnValue('ABCD')
+
     const result = middleware(new NextRequest('http://example.com/'))
-    // FIXME: replace with Content-Security-Policy
-    expect(result.headers.get('Content-Security-Policy-Report-Only')).toBeNull()
+    const cspHeader = result.headers.get('Content-Security-Policy-Report-Only')
+
+    expect(nonceGeneratorSpy).toHaveBeenCalledOnce()
+    expect(cspHeader).toBeString()
+    expect(cspHeader).toInclude(
+      "script-src 'self' https://matomo.incubateur.anct.gouv.fr 'nonce-ABCD' 'unsafe-eval';",
+    )
   })
 
-  // FIXME: Enable this test once Next 13 supports nonce feature on NextScript - See https://github.com/vercel/next.js/issues/42330
   it.skip('Adds CSP headers in production environment', () => {
     process.env = { NODE_ENV: 'production' }
+    nonceGeneratorSpy.mockReturnValue('1234')
+
     const result = middleware(new NextRequest('http://example.com/'))
-    // FIXME: replace with Content-Security-Policy
-    expect(result.headers.get('Content-Security-Policy-Report-Only')).toBe(
-      "default-src 'self' https://sentry.incubateur.net; script-src 'self' https://matomo.incubateur.anct.gouv.fr; script-src-attr 'none'; style-src 'self' https: 'unsafe-inline'; img-src 'self' data:; object-src 'none'; font-src 'self' https: data:; frame-ancestors 'self'; form-action 'self'; base-uri 'self'; upgrade-insecure-requests;",
+    const cspHeader = result.headers.get('Content-Security-Policy-Report-Only')
+    expect(nonceGeneratorSpy).toHaveBeenCalledOnce()
+    expect(cspHeader).toBeString()
+    expect(cspHeader).toInclude(
+      "script-src 'self' https://matomo.incubateur.anct.gouv.fr 'nonce-1234;",
     )
+    expect(cspHeader).not.toInclude('unsafe-eval')
+    expect(cspHeader).not.toInclude('unsafe-inline')
   })
 })
