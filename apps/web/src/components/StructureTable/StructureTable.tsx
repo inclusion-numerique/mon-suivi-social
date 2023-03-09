24692259e911cc5c5591941a21edbdedc6424e7e
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2023-03-09 15:41:20 +0100

    refactor: remove create query structure

--- apps/web/src/components/StructureTable/StructureTable.tsx
+++ apps/web/src/components/StructureTable/StructureTable.tsx
@@ -1,42 +1,40 @@
-import { asyncComponent } from '@mss/web/utils/asyncComponent'
 import { Routes } from '@mss/web/app/routing/routes'
 import { TableRowWithRowLink } from '@mss/web/components/Generic/table/TableRowWithRowLink'
-import type { QueryResult } from '@mss/web/features/createQuery.server'
-import { ListStructuresServer } from '@mss/web/features/structure/listStructures/listStructures.server'
 import { structureColumns } from './structureColumns'
+import { StructuresList } from '@mss/web/query/structure'
 
-export const StructureTable = asyncComponent(
-  async ({
-    structures,
-  }: Pick<QueryResult<typeof ListStructuresServer>, 'structures'>) => {
-    if (structures.length === 0) {
-      return (
-        <tr>
-          <td colSpan={structureColumns.length}>
-            Aucune structure ne correspond à votre recherche
-          </td>
-        </tr>
-      )
-    }
-
+export const StructureTable = ({
+  structures,
+}: {
+  structures: StructuresList
+}) => {
+  if (structures.length === 0) {
     return (
-      <>
-        {structures.map((structure) => {
-          const href = Routes.Structure.Index.path(structure)
-
-          const title = `Page de la structure ${structure.name}`
-
-          return (
-            <TableRowWithRowLink
-              key={structure.id}
-              item={structure}
-              columns={structureColumns}
-              href={href}
-              title={title}
-            />
-          )
-        })}
-      </>
+      <tr>
+        <td colSpan={structureColumns.length}>
+          Aucune structure ne correspond à votre recherche
+        </td>
+      </tr>
     )
-  },
-)
+  }
+
+  return (
+    <>
+      {structures.map((structure) => {
+        const href = Routes.Structure.Index.path(structure)
+
+        const title = `Page de la structure ${structure.name}`
+
+        return (
+          <TableRowWithRowLink
+            key={structure.id}
+            item={structure}
+            columns={structureColumns}
+            href={href}
+            title={title}
+          />
+        )
+      })}
+    </>
+  )
+}
