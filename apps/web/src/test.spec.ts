4bfc039996605356d74a3f5175588da5241b8d64
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2023-03-28 15:39:21 +0200

    chore: remove sample test

--- apps/web/src/test.spec.ts
+++
@@ -1,9 +0,0 @@
-import { theTestFunction } from './test'
-
-describe('test', () => {
-  it('Can test stuff', () => {
-    const result = theTestFunction()
-    expect(result.id).toBeString()
-    expect(result.hello).toEqual('world')
-  })
-})
